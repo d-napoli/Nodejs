35df58b1dfe8a5bf1725d72ec53d4c37f42d8c36
Diego Napoli <d.napoli.dev@gmail.com>
2021-01-30 09:49:21 -0300

    File Upload and Validation
    File Upload module and validation from the format

--- nodejs-rest/arquivos/uploadDeArquivos.js
+++ nodejs-rest/arquivos/uploadDeArquivos.js
@@ -1,6 +1,22 @@
 const fs = require('fs') // requiring file system
+const path = require('path') // lib nativa do node, serve para utilizar com arquivos e caminhos
 
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho) // descobrindo a extensão do arquivo
+    
+    // checando se a extensão do arquivo informado consta nos tipos válidos
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
+    
+    if(tipoEhValido) {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
 
-fs.createReadStream('./assets/salsisha.jpg')
-    .pipe( fs.createWriteStream('./assets/salsisha-stream.jpg'))
-    .on('finish', () => console.log('Imagem foi escrita com sucesso via stream'))
+        fs.createReadStream(caminho)
+            .pipe( fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(false, novoCaminho))
+    } else {
+        const erro = `Extensão de arquivo ${tipo} é inválida`
+        console.log('Erro! Tipo de arquivo inválido')
+        callbackImagemCriada(erro)
+    }
+}
---
+++ nodejs-rest/models/pets.js
@@ -0,0 +1,26 @@
+const conexao = require('../infraestrutura/conexao')
+const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
+
+class Pet {
+    adiciona(pet, res) { // pet vai ser um objeto informado pelo usuário
+        const query = 'INSERT INTO Pets SET ?' // Prepared Statement
+
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                const novoPet = {nome: pet.nome, imagem: novoCaminho}
+
+                conexao.query(query, novoPet, erro => {
+                    if(erro) {
+                        res.status(400).json({ erro })
+                    } else {
+                        res.status(200).json(novoPet)
+                    }
+                })
+            }
+        })
+    }
+}
+
+module.exports = new Pet()
