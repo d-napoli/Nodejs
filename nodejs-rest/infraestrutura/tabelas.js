916865c40f77904dfefd1aec0de2f02c14d0bd91
Diego Napoli <d.napoli.dev@gmail.com>
2021-01-31 08:55:49 -0300

    Client External Faker API
    Consuming a external Client Faker API

--- nodejs-rest/infraestrutura/tabelas.js
+++ nodejs-rest/infraestrutura/tabelas.js
@@ -6,7 +6,7 @@ class Tabelas {
     }
 
     criarAtendimentos() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime not null, status varchar(20) not null, observacoes text, primary key(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente VARCHAR(11) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime not null, status varchar(20) not null, observacoes text, primary key(id))'
 
         this.conexao.query(sql, erro => {
             if(erro) {
