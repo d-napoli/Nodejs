79a0b42bb3f2f4fb2fd3a23e7fe7ea4de10d0fba
Diego Napoli <d.napoli.dev@gmail.com>
2021-02-23 20:26:33 -0300

    Autenticação com Login

---
+++ BlogDoCodigo/src/usuarios/usuarios-controlador.js
@@ -0,0 +1,57 @@
+const Usuario = require('./usuarios-modelo');
+const { InvalidArgumentError, InternalServerError } = require('../erros');
+
+const jwt = require('jsonwebtoken')
+
+function criaTokenJWT(usuario) {
+  const payload = { id: usuario.id }
+  const token = jwt.sign(payload, process.env.CHAVE_JWT, { expiresIn: '15m' }) // puxa a chave usando o arquivo .env
+  return token
+}
+
+module.exports = {
+  adiciona: async (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    try {
+      const usuario = new Usuario({
+        nome,
+        email
+      });
+
+      await usuario.adicionaSenha(senha)
+      await usuario.adiciona();
+
+      res.status(201).json();
+    } catch (erro) {
+      if (erro instanceof InvalidArgumentError) {
+        res.status(422).json({ erro: erro.message });
+      } else if (erro instanceof InternalServerError) {
+        res.status(500).json({ erro: erro.message });
+      } else {
+        res.status(500).json({ erro: erro.message });
+      }
+    }
+  },
+
+  login: (req, res) => {
+    const token = criaTokenJWT(req.user) // variavel user é criada na hora da autenticação, NÃO é informada pelo usuário"
+    // vamos colocar o token no cabeçalho da resposta
+    res.set('Authorization', token).status(204).send()
+  },
+
+  lista: async (req, res) => {
+    const usuarios = await Usuario.lista();
+    res.json(usuarios);
+  },
+
+  deleta: async (req, res) => {
+    const usuario = await Usuario.buscaPorId(req.params.id);
+    try {
+      await usuario.deleta();
+      res.status(200).send();
+    } catch (erro) {
+      res.status(500).json({ erro: erro });
+    }
+  }
+};
