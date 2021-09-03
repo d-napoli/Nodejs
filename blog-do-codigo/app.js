b6012fd4fa055d2eb79c31e31b63c70f58e31097
Diego Napoli <37269543+d-napoli@users.noreply.github.com>
2021-09-03 08:02:07 -0300

    Deleting blog do codigo

--- blog-do-codigo/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const  { estrategiasAutenticacao } = require('./src/usuarios')
-
-app.use(bodyParser.json())
-
-module.exports = app;
